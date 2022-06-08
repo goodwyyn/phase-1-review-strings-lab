38dac6cf46d23e12e6bca76a29250c0f694852e9
goodwyyn <godwin.mtuma@student.moringaschool.com>
2022-06-08 14:39:45 +0300

    strings lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your code in this file!
+
+
+const currentUser = 'Godwin Mutuma';
+
+
+const welcomeMessage = `'Welcome to Flatbook, ' + ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome,  ${currentUser .slice (0, 1)}!`;
+
+'Godwin Mutuma' .length
+
+
+const myString = 'template literal';
+
+const myNumber = 10 ;
+
+const myBoolean = false;
+
+`saying that the interpolation with ${myString}s is better then concantenation ${90 + myNumber}% of the time is simply ${myBoolean}. But it is pretty cool!
+Beware that the new lines inside of a ${myString} will be preserved as new line in the resulting ${typeof myString}!`;
+
+
+
